980e6cc547296c8796272a4229475212cae37f14
Kawashima-Tomoya <57kawashima@gmail.com>
2023-06-18 13:27:07 +0900

    ⚗️ クリックイベントの実験（いつか消す）

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,23 @@
 import Head from "next/head";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
+import { useCallback } from "react";
+
+// const handleClick = (e) => {
+//   console.log(e.target.href);
+//   e.preventDefault();
+// };
 
 export default function Home() {
+  const foo = 1;
+  /* useCallback(関数, 依存配列)はサイレンダリング時の関数の再生性を防げるため、
+   関数をコンポーネントの内部に記述してもパフォーマンスを保てる */
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <>
       <Head>
@@ -10,6 +25,9 @@ export default function Home() {
       </Head>
 
       <Header />
+      <a href="./about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
     </>
   );
