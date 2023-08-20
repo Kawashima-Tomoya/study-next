0d3ea886c757ddd6d2efbb3be217dfadbcf15fda
Kawashima-Tomoya <57kawashima@gmail.com>
2023-08-20 15:41:19 +0900

    🔥 Indexから不要なコードを削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,37 +1,14 @@
 import Head from "next/head";
-import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
-const About = (props) => {
+const Index = () => {
   return (
     <>
       <Head>
-        <title>About Page</title>
+        <title>Index Page</title>
       </Head>
-
       <Header />
-
-      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-
-      <input
-        type="text"
-        name="name"
-        value={props.text}
-        onChange={props.handleChange}
-      />
-      <button onClick={props.handleAdd}>追加</button>
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-
-      <Main page="about" />
     </>
   );
 };
 
-export default About;
+export default Index;
