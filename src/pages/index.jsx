44f5aea917a00878d16f6f167e24d3f612071cef
Kawashima-Tomoya <57kawashima@gmail.com>
2023-09-09 16:38:38 +0900

    💄 max-width600pxと中央揃え

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,17 @@
 import Head from "next/head";
 import { Header } from "@/src/components/Header";
+import styles from "@/src/styles/Home.module.css";
+
 const Index = () => {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-    </>
+      <h1>Next.js</h1>
+      <p>テキストテキストテキストテキスト</p>
+    </div>
   );
 };
 
