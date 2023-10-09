29b615b70d4b2e6ca6ca67c08c571bf9aaa00385
Kawashima-Tomoya <57kawashima@gmail.com>
2023-10-09 18:09:17 +0900

    🎨 APIのURLをconstから読み込むように変更

--- src/hooks/useComment.jsx
+++ src/hooks/useComment.jsx
@@ -1,13 +1,12 @@
 const { useRouter } = require("next/router");
+import { API_URL } from "@/src/utils/const";
 import { fetcher } from "@/src/utils/fetcher";
 import useSWRImmutable from "swr/immutable";
 
 export const useComment = () => {
   const router = useRouter();
   const { data, error, isLoading } = useSWRImmutable(
-    router.query.id
-      ? `https://jsonplaceholder.typicode.com/comments/${router.query.id}`
-      : null,
+    router.query.id ? `${API_URL}comments/${router.query.id}` : null,
     fetcher
   );
 
--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -1,13 +1,12 @@
 const { fetcher } = require("@/src/utils/fetcher");
 const { useRouter } = require("next/router");
+import { API_URL } from "@/src/utils/const";
 import useSWRImmutable from "swr/immutable";
 
 export const useUser = () => {
   const router = useRouter();
   const { data, error, isLoading } = useSWRImmutable(
-    router.query.id
-      ? `https://jsonplaceholder.typicode.com/users/${router.query.id}`
-      : null,
+    router.query.id ? `${API_URL}users/${router.query.id}` : null,
     fetcher
   );
 
